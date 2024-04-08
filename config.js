3c08741ea6359bf9bf90d5db1c009e0e5d954340
ghost20008 <138211590+ghost20008@users.noreply.github.com>
2024-04-08 12:57:23 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['0787199041', 'Ghost', true],
+  ['0787199041', 'Thandeka', true], 
   [''] 
 ] //Number of owners
 
